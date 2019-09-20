80af085935789a2bb8f8d2bada754742f4848b32
thierrygribeauval <t.gribeauval11@gmail.com>
2019-09-20 11:30:20 -0400

    third commit

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -92,9 +92,8 @@ describe('Lotteray Contract', () => {
         await lottery.methods.pickWinner().send({ from: accounts[0] });
 
         const finalBalance = await web3.eth.getBalance(accounts[0]);
-
         const difference = finalBalance - initialBalance;
-        console.log(finalBalance - initialBalance);
+        
         assert(difference > web3.utils.toWei('1.8', 'ether'));
     })
 });
